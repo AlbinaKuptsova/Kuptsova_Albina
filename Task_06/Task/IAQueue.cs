4f6f9d2c904933bfacab7a0cfe26216772e20d99
Albina Kuptsova <binnnnna@mail.ru>
2016-01-30 23:46:49 +0300

    Commented

--- Task_06/Task/IAQueue.cs
+++ Task_06/Task/IAQueue.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Task
 {
+    /// <summary>
+    /// Generic interface for Queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IAQueue<T>: IEnumerable<T>, ICloneable, IDisposable
     {
         void Enqueue(T data);
